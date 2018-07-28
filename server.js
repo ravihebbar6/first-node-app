d95ffeaaffe6516b1e1b5054ac0778d045487f1f
Ravi Hebbar <rhebbar@teamdrg.com>
2018-07-28 21:16:41 +0530

    First commit to my first node app

---
+++ server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+var serverAttr = function(req, res) {
+	pathname = url.parse(req.url).pathname;
+	console.log(`pathname = ${pathname}`);
+
+	params = querystring.parse(url.parse(req.url).query);
+	console.log(`params %j`, params);
+
+	res.writeHead(200, {'Content-Type': 'text/HTML'});
+	if (pathname == '/')
+		res.write(`<h1>You are in root Mr. ${params['name']}</h1>`);
+	else if (pathname == '/home')
+		res.write('<h1>You are in home</h1>');
+	else
+		res.write('<h1>You are in unkown place</h1>')
+	res.end();
+};
+
+var server = http.createServer(serverAttr);
+
+server.listen(8080);
